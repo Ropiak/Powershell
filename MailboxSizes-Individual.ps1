59becdd5281bfaa465041b705887bbba5fbf2c66
Travis <Scine@users.noreply.github.com>
2016-09-01 11:24:47 -0700

    Update MailboxSizes-Individual.ps1

--- MailboxSizes-Individual.ps1
+++ MailboxSizes-Individual.ps1
@@ -1,3 +1,5 @@
+##For getting mailbox sizes for individuals in your Office 365 environment.##
+
 $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session
